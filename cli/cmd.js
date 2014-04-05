e4c3fbc190d419797d9a222af3cdcfc57ac25269
James Newell <james@digitaledgeit.com.au>
2014-04-05 14:02:15 +1100

    return an exit code on error

--- cli/cmd.js
+++ cli/cmd.js
@@ -1,15 +1,16 @@
 #!/usr/bin/env node
 
-var recursiveUglifyJS = require('../index');
+var recursiveCSSMin = require('../index');
 
 if (process.argv.length != 3) {
 	console.log('recursive-uglifyjs: Please enter a (single) directory of scripts to uglify.');
-	process.exit();
+	process.exit(1);
 }
 
-recursiveUglifyJS(process.argv[2]).then(function() {
+recursiveCSSMin(process.argv[2]).then(function() {
 	console.log('done!');
 }, function() {
 	console.log('error!');
+	process.exit(1);
 });
 
