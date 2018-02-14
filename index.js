5938658c256b042baa7c7ae8320cef1241cd9bec
Greg Bell <greg@bitwombat.com.au>
2018-02-14 15:57:27 +1100

    Handle errors properly.  Use uglifycss instead.

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 var fs = require('fs');
-var minify = require('cssmin');
+var minify = require('uglifycss');
 var finder = require('finder-on-steroids');
 var Promise = require('promise');
 
 var readfile = Promise.denodeify(fs.readFile);
 var writefile = Promise.denodeify(fs.writeFile);
+
+function rejected(arg) {
+    console.log('REJECTED: ' + arg);
+    console.log(arg);
+    return Promise.reject('Had an error somewhere');
+}
 
 /**
  * Minifies all CSS files in a given folder
@@ -13,15 +19,14 @@ var writefile = Promise.denodeify(fs.writeFile);
  */
 function recursiveCSSMin(directory) {
 
-	return finder(directory).files().name('*.css').find().then(function(styles) {
-		return Promise.all(styles.map(function(style) {
-			return readfile(style, {encoding: 'utf-8'}).then(function(data) {
-				return writefile(style, minify(data)).then(function() {
-					console.log('Minified '+style);
-				});
-			});
-		}));
-	});
+    return finder(directory).files().name('*.css').find().then(function(styles) {
+            return Promise.all(styles.map(function(style) {
+                var min = minify.processFiles([style]);
+                return writefile(style, minify.processFiles([style])).then(function() {
+                    console.log('Minified ' + style);
+                    });
+            }));
+    }).then(null, rejected);
 
 }
 
