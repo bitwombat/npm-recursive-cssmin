8f4c0a933b5656c136d2905889054b4ecc6a68a5
Greg Bell <greg@bitwombat.com.au>
2019-05-09 08:04:50 +1000

    Use ProgressBar

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
 var fs = require('fs');
 var minify = require('uglifycss');
 var finder = require('finder-on-steroids');
+var ProgressBar = require('progress');
 
 /**
  * Minifies all CSS files in a given folder
  * @param   {string} directory
  */
 function recursiveCSSMin(directory) {
-    console.log('Reading filenames');
-    return finder(directory).files().name('*.css').find().then(function(styles) {
-            console.log("Uglifycss'ing the files");
-            // Do everything synchronously to work around running out of file handles problem
-            styles.forEach(function(style) {
-                    console.log(style);
-                    fs.writeFileSync(style, minify.processFiles([style]));
-                    });
+    console.log( 'Finding all CSS files' );
+    return finder(directory).files().name( '*.css' ).find(launch);
+}
 
-            });
+function launch(err, files) {
+    var bar = new ProgressBar('[:bar] Minified :current of :total files, :percent, :rate/fps, :etas', {
+        complete: '=',
+        incomplete: ' ',
+        width: 20,
+        total: files.length,
+        });
+
+    // Do everything synchronously to work around running out of file handles problem
+    files.forEach(function(style) {
+        fs.writeFileSync(style, minify.processFiles([style]));
+        bar.tick();
+    });
 }
 
 module.exports = recursiveCSSMin;
